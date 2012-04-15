639d365d3a1fadadcdbc8b1c7cd1f35e1e24174c
Will Whitney <wfwhitney@gmail.com>
2012-04-14 22:13:41 -0400

    removed pre-swiper code and refactored

--- src/suggestcorp/suggestible/CardActivity.java
+++ src/suggestcorp/suggestible/CardActivity.java
@@ -16,7 +16,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
-public class CardActivity extends Activity implements OnGestureListener {
+public class CardActivity extends Activity {
 
 	LocationManager locManager;
 	Location location;
@@ -24,18 +24,18 @@ public class CardActivity extends Activity implements OnGestureListener {
 	LinearLayout linear;
 	GestureDetector gdetect;
 	int screen = 0;
-	uk.co.jasonfry.android.tools.ui.SwipeView flipper;
+	uk.co.jasonfry.android.tools.ui.SwipeView swiper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.card_layout);
 
         txt = (TextView) findViewById(R.id.locText);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
         txt.setTypeface(font);
 
-        flipper = (uk.co.jasonfry.android.tools.ui.SwipeView) findViewById(R.id.flipper);
+        swiper = (uk.co.jasonfry.android.tools.ui.SwipeView) findViewById(R.id.flipper);
         
 
         linear = (LinearLayout) findViewById(R.id.card);
@@ -48,94 +48,8 @@ public class CardActivity extends Activity implements OnGestureListener {
 
         txt.setText("Sushi sounds great right now.");
 
-        gdetect = new GestureDetector(this, this);
-
-
-
-//        txt.setText( String.format("latitude: %f, longitude: %f", location.getLatitude(), location.getLongitude()));
-
-
 
     }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-    	this.gdetect.onTouchEvent(event);
-    	return super.onTouchEvent(event);
-    }
-
-	public boolean onDown(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public boolean onFling(MotionEvent arg0, MotionEvent arg1, float xvel,
-			float yvel) {
-				
-		if(Math.abs(xvel) > Math.abs(yvel)) {
-			
-			RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) linear.getLayoutParams();
-
-
-			if(xvel > 0) {
-				
-				// This isn't correct, but want to set the animations based on which direction we're going
-				//flipper.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left));
-				
-				if(screen > -1) {
-					if (screen == 0)
-						txt.setText("Previous!");
-					if (screen == 1)
-						txt.setText("Sushi sounds great right now.");
-					screen--;
-				}
-			} else {
-				if(screen < 1) {
-					if (screen == 0)
-						txt.setText("Next!");
-					if (screen == -1)
-						txt.setText("Sushi sounds great right now.");
-					screen++;
-				}
-			}
-			
-			
-			params.leftMargin = 15;
-			params.rightMargin = 15;
-			linear.setLayoutParams(params);
-			return true;
-		}
-
-		return false;
-	}
-
-	public void onLongPress(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float x,
-			float y) {
-		// need to add handling for when it's released
-		// if it's more than halfway, animate to the next one, otherwise animate back
-		// may not be able to do that in onScroll
-		RelativeLayout.LayoutParams params = (android.widget.RelativeLayout.LayoutParams) linear.getLayoutParams();
-		params.leftMargin -= (int) (x);
-		params.rightMargin += (int) (x);
-		linear.setLayoutParams(params);
-		return true;
-	}
-
-	public void onShowPress(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public boolean onSingleTapUp(MotionEvent arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 
 
 }
