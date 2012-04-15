8d9ebc45ea660ba604d1130364593b2899fcd921
Will Whitney <wfwhitney@gmail.com>
2012-04-15 17:20:28 -0400

    new cards created on the fly

--- src/suggestcorp/suggestible/CardActivity.java
+++ src/suggestcorp/suggestible/CardActivity.java
@@ -1,55 +1,120 @@
 package suggestcorp.suggestible;
 
+import uk.co.jasonfry.android.tools.ui.SwipeView.OnPageChangedListener;
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Typeface;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
-import android.view.MotionEvent;
-import android.view.animation.AnimationUtils;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
-import android.widget.ViewFlipper;
 
 public class CardActivity extends Activity {
 
 	LocationManager locManager;
 	Location location;
 	TextView txt;
-	LinearLayout linear;
-	GestureDetector gdetect;
-	int screen = 0;
+	RelativeLayout card1;
 	uk.co.jasonfry.android.tools.ui.SwipeView swiper;
+	OnDemandCardmaker cardmaker;
+	final Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_layout);
-
-        txt = (TextView) findViewById(R.id.locText);
-        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
-        txt.setTypeface(font);
-
+        Log.d("Suggestible", "I'm running!");
+        
+        cardmaker = new OnDemandCardmaker();
+        txt = (TextView) findViewById(R.id.txt);
         swiper = (uk.co.jasonfry.android.tools.ui.SwipeView) findViewById(R.id.flipper);
+        card1 = (RelativeLayout) findViewById(R.id.card1);
+        
+//        final Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+//        txt.setTypeface(font);
+		final ViewGroup mContainer = (ViewGroup) findViewById(android.R.id.content).getRootView();
+		CardActivity.setAppFont(mContainer, font);
+		swiper.setOnPageChangedListener(cardmaker);
         
 
-        linear = (LinearLayout) findViewById(R.id.card);
+    }
+    
+    private class OnDemandCardmaker implements OnPageChangedListener {
 
+		public void onPageChanged(int oldpage, int newpage) {
 
-        Log.d("Suggestible", "I'm running!");
+			txt = (TextView) findViewById(R.id.txt);
+			txt.setText("fuck off");
+			if (newpage > oldpage) {
+//				Log.d("Suggestible", card1.getLayoutParams().toString());
+//				LayoutParams params = (android.widget.RelativeLayout.LayoutParams) card1.getLayoutParams();
+				View newCard = new View(CardActivity.this);
+				newCard.inflate(CardActivity.this, R.layout.card, (ViewGroup) swiper);
+				final ViewGroup mContainer = (ViewGroup) findViewById(android.R.id.content).getRootView();
+				CardActivity.setAppFont(mContainer, font);
 
-        locManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        location = locManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+				
+			}
+			
+		}
+    	
+    }
+    
+    
+    
+    
+    
+    /**
+     * Recursively sets a typeface to all TextViews in a ViewGroup.
+     */
+    public static final void setAppFont(ViewGroup mContainer, Typeface mFont)
+    {
+        if (mContainer == null || mFont == null) return;
 
-        txt.setText("Sushi sounds great right now.");
+        final int mCount = mContainer.getChildCount();
 
-
+        // Loop through all of the children.
+        for (int i = 0; i < mCount; ++i)
+        {
+            final View mChild = mContainer.getChildAt(i);
+            if (mChild instanceof TextView)
+            {
+                // Set the font if it is a TextView.
+                ((TextView) mChild).setTypeface(mFont);
+            }
+            else if (mChild instanceof ViewGroup)
+            {
+                // Recursively attempt another ViewGroup.
+                setAppFont((ViewGroup) mChild, mFont);
+            }
+        }
     }
 
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
---
+++ src/suggestcorp/suggestible/CardLayout.java
@@ -0,0 +1,23 @@
+package suggestcorp.suggestible;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class CardLayout extends RelativeLayout {
+
+	public CardLayout(Context context) {
+		super(context);
+	}
+	
+	public CardLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public CardLayout(Context context, AttributeSet attrs, int a) {
+		super(context, attrs, a);
+		// TODO Auto-generated constructor stub
+	}
+
+}
