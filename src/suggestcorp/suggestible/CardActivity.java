485e38d72802600babe891b6035f99805760dcab
Will Whitney <wfwhitney@gmail.com>
2012-04-15 20:41:38 -0400

    dynamically generating content from resources

--- src/suggestcorp/suggestible/CardActivity.java
+++ src/suggestcorp/suggestible/CardActivity.java
@@ -1,5 +1,7 @@
 package suggestcorp.suggestible;
 
+import java.lang.reflect.Field;
+
 import uk.co.jasonfry.android.tools.ui.SwipeView.OnPageChangedListener;
 import android.app.Activity;
 import android.graphics.Typeface;
@@ -9,8 +11,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 public class CardActivity extends Activity {
@@ -21,6 +24,7 @@ public class CardActivity extends Activity {
 	RelativeLayout card1;
 	uk.co.jasonfry.android.tools.ui.SwipeView swiper;
 	OnDemandCardmaker cardmaker;
+	final int MAX_PAGES = 4;
 	
 
     @Override
@@ -31,34 +35,57 @@ public class CardActivity extends Activity {
         Log.d("Suggestible", "I'm running!");
         
         cardmaker = new OnDemandCardmaker();
-        txt = (TextView) findViewById(R.id.txt);
         swiper = (uk.co.jasonfry.android.tools.ui.SwipeView) findViewById(R.id.flipper);
-        card1 = (RelativeLayout) findViewById(R.id.card1);
         
-        final Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
-//        txt.setTypeface(font);
-		final ViewGroup mContainer = (ViewGroup) findViewById(android.R.id.content).getRootView();
-		CardActivity.setAppFont(mContainer, font);
+        
+//        int textToSet = getResources().getIdentifier("card" + 0, "string", "suggestcorp.suggestible");
+//		getTextViewByPageNum(0).setText(textToSet);
+//		
+//		int imageToSet = getResources().getIdentifier("card" + 0, "drawable", "suggestcorp.suggestible");
+//		getImageViewByPageNum(0).setImageResource(imageToSet);
+
+        
+//        card1 = (RelativeLayout) findViewById(R.id.card1);
+//        RelativeLayout card2 = (RelativeLayout) findViewById(R.id.card2);
+//        Toast.makeText(this, "index 1: " + ((ViewGroup) ((ViewGroup) ((ViewGroup) swiper.getChildAt(0)).getChildAt(0)).getChildAt(0)).getChildAt(0).getClass(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "card2 index: " + swiper.indexOfChild(card2), Toast.LENGTH_SHORT).show();
+        
+        
+        fixFonts();
+		
 		swiper.setOnPageChangedListener(cardmaker);
         
+		cardmaker.onPageChanged(-1, 0);
 
     }
     
     private class OnDemandCardmaker implements OnPageChangedListener {
 
 		public void onPageChanged(int oldpage, int newpage) {
-
-			txt = (TextView) findViewById(R.id.txt);
-			txt.setText("fuck off");
+			
 			if (newpage > oldpage) {
-//				Log.d("Suggestible", card1.getLayoutParams().toString());
-//				LayoutParams params = (android.widget.RelativeLayout.LayoutParams) card1.getLayoutParams();
-				View newCard = new View(CardActivity.this);
-				newCard.inflate(CardActivity.this, R.layout.card, (ViewGroup) swiper);
+								
+				int textToSet = getResources().getIdentifier("card" + newpage, "string", "suggestcorp.suggestible");
+				getTextViewByPageNum(newpage).setText(textToSet);
+				
+				int imageToSet = getResources().getIdentifier("card" + newpage, "drawable", "suggestcorp.suggestible");
+				getImageViewByPageNum(newpage).setImageResource(imageToSet);
+
+				if (swiper.getPageCount() < MAX_PAGES) {
+					View newCard = new View(CardActivity.this);
+					newCard.inflate(CardActivity.this, R.layout.card, (ViewGroup) swiper);
+				}
+				
 
 				final ViewGroup mContainer = (ViewGroup) findViewById(android.R.id.content).getRootView();
 				final Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
 				CardActivity.setAppFont(mContainer, font);
+				
+				
+				
+
+				
+				
 				
 				// THIS IS HOW YOU ASSIGN STRINGS AS IDs
 				// http://stackoverflow.com/questions/3937010/array-of-imagebuttons-assign-r-view-id-from-a-variable
@@ -100,6 +127,75 @@ public class CardActivity extends Activity {
                 setAppFont((ViewGroup) mChild, mFont);
             }
         }
+    }
+    
+    
+    
+    public final void fixFonts() {
+    	
+        final Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Thin.ttf");
+		final ViewGroup mContainer = (ViewGroup) findViewById(android.R.id.content).getRootView();
+		CardActivity.setAppFont(mContainer, font);
+
+    }
+    
+    
+    
+    public TextView getTextViewByPageNum(int page) {
+    	
+    	RelativeLayout card = (RelativeLayout) ((ViewGroup) swiper.getChildAt(0)).getChildAt(page);
+    	
+    	return getTextViewChild(card);
+    }
+    
+    public ImageView getImageViewByPageNum(int page) {
+    	
+    	RelativeLayout card = (RelativeLayout) ((ViewGroup) swiper.getChildAt(0)).getChildAt(page);
+    	
+    	return getImageViewChild(card);
+    }
+    
+    public RelativeLayout getCardByPageNum(int page) {
+    	return (RelativeLayout) ((ViewGroup) swiper.getChildAt(0)).getChildAt(page);
+    }
+    
+    public TextView getTextViewChild(ViewGroup group) {
+    	int childCount = group.getChildCount();
+    	
+    	for (int i = 0; i < childCount; i++) {
+    		View child = group.getChildAt(i);
+    		if (child instanceof TextView) {
+    			return (TextView) child;
+    		} else if (child instanceof ViewGroup) {
+    			return getTextViewChild((ViewGroup) child);
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+    public ImageView getImageViewChild(ViewGroup group) {
+    	int childCount = group.getChildCount();
+    	
+    	for (int i = 0; i < childCount; i++) {
+    		View child = group.getChildAt(i);
+    		if (child instanceof ImageView) {
+    			return (ImageView) child;
+    		} else if (child instanceof ViewGroup) {
+    			return getImageViewChild((ViewGroup) child);
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+    public Button[] getCardButtonsByPageNum(int page) {
+    	Button[] buttons = new Button[2];
+    	
+    	buttons[0] = (Button) getCardByPageNum(page).findViewWithTag("first");
+    	buttons[0] = (Button) getCardByPageNum(page).findViewWithTag("second");
+    	
+    	return buttons;
     }
 
 
