9e842d555eaa6ef582949dcfa13fcce1471a467d
Cristina <clozano@mit.edu>
2012-05-10 19:19:01 -0400

    merging again

--- src/suggestcorp/suggestible/GetServerResponse.java
+++ src/suggestcorp/suggestible/GetServerResponse.java
@@ -1,6 +1,16 @@
 package suggestcorp.suggestible;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class GetServerResponse {
 
@@ -16,6 +26,75 @@ public class GetServerResponse {
 
         }
     }
+    
+    public static List<Suggestion> getMovies() {
+    	String url = "http://afternoon-planet-7936.herokuapp.com/movies";
+    	String json = getURLContents(url);
+    	
+    	Gson gson = new Gson();
+    	Type collectionType = new TypeToken<List<Suggestion>>(){}.getType();
+    	List<Suggestion> suggestions = gson.fromJson(json, collectionType);
+    	
+    	
+    	
+    	return suggestions;
+    }
+    
+    
+    public static List<Suggestion> getBooks() {
+    	String url = "http://afternoon-planet-7936.herokuapp.com/books";
+    	String json = getURLContents(url);
+    	
+    	Gson gson = new Gson();
+    	Type collectionType = new TypeToken<List<Suggestion>>(){}.getType();
+    	List<Suggestion> suggestions = gson.fromJson(json, collectionType);
+    	
+    	return suggestions;
+    }
+    
+    public static List<Suggestion> getRestaurants(float lat, float lon) {
+    	String url = "http://afternoon-planet-7936.herokuapp.com/restaurants?lat=" + lat + "&lon=" + lon;
+    	String json = getURLContents(url);
+    	
+    	Gson gson = new Gson();
+    	Type collectionType = new TypeToken<List<Suggestion>>(){}.getType();
+    	List<Suggestion> suggestions = gson.fromJson(json, collectionType);
+    	
+    	return suggestions;
+    }
+    
+ 
+    public static String getURLContents(String url) {
+        
+		try {
+			
+			URL target = new URL(url);
+		    URLConnection conn = target.openConnection();
+		    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		    
+		    
+		    String inputLine;
+		    String output = "";
+		    while ((inputLine = in.readLine()) != null) {
+		    	
+		        output += inputLine;
+		        
+		    }
+		    in.close();
+		    
+		    return output;
+		    
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+    }
+
 
     /**
      * 
