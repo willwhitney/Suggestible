61a2dd7761e17d99a3e289d0a7953bea7e8d43cb
Cristina <clozano@mit.edu>
2012-05-10 19:15:24 -0400

    changes to movie layout and undelete

--- src/suggestcorp/suggestible/MovieInfoActivity.java
+++ src/suggestcorp/suggestible/MovieInfoActivity.java
@@ -2,14 +2,35 @@ package suggestcorp.suggestible;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MovieInfoActivity extends Activity {
-	public void onCreate(Bundle savedInstanceState) {
-		
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.movielayout);
-		
-		
-	}
-	
+    String title = "Ironman";
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.movielayout);
+
+        Button theaterButton = (Button) findViewById(R.id.theaterbutton);
+        theaterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        Button imdbButton = (Button) findViewById(R.id.imdbbutton);
+        imdbButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("imdb", "CLICK");
+                String url = "http://www.imdbapi.com/?t=" + title;   
+            }
+        });
+
+    }
+
 }
