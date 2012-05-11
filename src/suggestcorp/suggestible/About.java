68a73f0f8c3ff153a14a9c17bbe050b770ebbd6b
roxman007 <michael.phox@gmail.com>
2012-05-11 03:30:06 -0400

    restaurant layout

--- src/suggestcorp/suggestible/About.java
+++ src/suggestcorp/suggestible/About.java
@@ -1,5 +1,6 @@
 package suggestcorp.suggestible;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 
--- src/suggestcorp/suggestible/MovieInfoActivity.java
+++ src/suggestcorp/suggestible/MovieInfoActivity.java
@@ -8,7 +8,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import com.google.gson.Gson;
 
 
 import android.app.Activity;
@@ -26,7 +26,68 @@ import android.widget.TextView;
 public class MovieInfoActivity extends Activity {
 
 	
-	
+	 public void onCreate(Bundle savedInstanceState) {
+
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.movielayout);
+
+	        Button theaterButton = (Button) findViewById(R.id.theaterbutton);
+	        theaterButton.setOnClickListener(new OnClickListener() {
+	            @Override
+	            public void onClick(View v) {
+	                String titlePlus = title.replace(" ", "+");
+	                String url = "http://www.google.com/movies?q="+titlePlus+"&btnG=Search+Movies&near=02139";
+	                final Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
+	                MovieInfoActivity.this.startActivity(intent);
+	            }
+	        });
+
+	        Button imdbButton = (Button) findViewById(R.id.imdbbutton);
+	        imdbButton.setOnClickListener(new OnClickListener() {
+	            @Override
+	            public void onClick(View v) {
+	                String titlePlus = title.replace(" ", "+");
+	                String url = "http://www.imdbapi.com/?t=" + titlePlus; 
+	                String json = GetServerResponse.getURLContents(url); 
+	                Gson gson = new Gson();
+	                String id = gson.fromJson("imdbID", String.class);
+	                url = "http://www.imdb.com/"+ id;
+	                final Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
+	                MovieInfoActivity.this.startActivity(intent);
+	            }
+	        });
+	        
+	        /*
+	        String title = getIntent().getStringExtra("title");
+	        ((TextView) findViewById(R.id.txtmpaa)).setText(title);
+	        
+	        String mpaa = getIntent().getStringExtra("mpaa");
+	        ((TextView) findViewById(R.id.txtmpaa)).setText(mpaa);
+	        
+	        Double rating = getIntent().getDoubleExtra("rating",0.0);
+	        fillStars(rating);
+	        
+	        String runtime = getIntent().getStringExtra("runtime");
+	        ((TextView) findViewById(R.id.txttime)).setText(runtime + " min");
+	        
+	        String description = getIntent().getStringExtra("description");
+	        ((TextView) findViewById(R.id.descriptionLarge)).setText(mpaa);
+	        ((TextView) findViewById(R.id.descriptionSmall)).setText(mpaa);
+	        */
+	        
+	        /* not functional at moment
+	         * 
+	        Button moreButton = (Button) findViewById(R.id.showMoreButton);
+	        moreButton.setOnClickListener(new OnClickListener() {
+	            @Override
+	            public void onClick(View v) {
+	                TextView description = (TextView) findViewById(R.id.descriptionSmall);
+	                description.setHeight(LayoutParams.WRAP_CONTENT);
+	            }
+	        });
+	*/
+	    }
+	 
 	public void showMore(View view){		
     	((Button)(findViewById(R.id.showMoreButton))).setVisibility(8);//make it gone
     	((TextView)(findViewById(R.id.descriptionSmall))).setVisibility(8);//make it gone
@@ -34,7 +95,7 @@ public class MovieInfoActivity extends Activity {
     	
     }
 	
-	public void fillStars(int rating){
+	public void fillStars(double rating){
 		double stars = rating/20.00;
 		if (stars >= 1){
 			((View)(findViewById(R.id.star1))).setBackgroundResource(R.drawable.full_star);
@@ -72,48 +133,7 @@ public class MovieInfoActivity extends Activity {
 
     String title = "Hunger Games";
 
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.movielayout);
-
-        Button theaterButton = (Button) findViewById(R.id.theaterbutton);
-        theaterButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String titlePlus = title.replace(" ", "+");
-                String url = "http://www.google.com/movies?q="+titlePlus+"&btnG=Search+Movies&near=02139";
-                final Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
-                MovieInfoActivity.this.startActivity(intent);
-            }
-        });
-
-        Button imdbButton = (Button) findViewById(R.id.imdbbutton);
-        imdbButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String titlePlus = title.replace(" ", "+");
-                String url = "http://www.imdbapi.com/?t=" + titlePlus; 
-                String json = GetServerResponse.getURLContents(url); 
-                Gson gson = new Gson();
-                String id = gson.fromJson("imdbID", String.class);
-                url = "http://www.imdb.com/"+ id;
-                final Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
-                MovieInfoActivity.this.startActivity(intent);
-            }
-        });
-        /* not functional at moment
-         * 
-        Button moreButton = (Button) findViewById(R.id.showMoreButton);
-        moreButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView description = (TextView) findViewById(R.id.descriptionSmall);
-                description.setHeight(LayoutParams.WRAP_CONTENT);
-            }
-        });
-*/
-    }
+   
 
 
 }
